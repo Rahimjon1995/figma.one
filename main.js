0a29c45ec994b726b42a56a1418f425bf11b645f
Rahimjon1995 <79519853+Rahimjon1995@users.noreply.github.com>
2021-03-14 14:13:23 +0300

    o'rta qismi

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+const sidebarItem = document.querySelectorAll('#item');
+
+sidebarItem.forEach((item, index, array) => {
+    item.addEventListener('click', (e) => {
+        if(!item.classList.contains('active')){
+            item.classList.add('active');
+         }else{
+            item.classList.remove('active');
+         }
+    });
+});
